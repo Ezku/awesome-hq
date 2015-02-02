7271a0559ade51211d5230b3338e5d89253c1421
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2015-02-02 21:28:26 +0200

    clearer interaction design

--- js/views/queue.js
+++ js/views/queue.js
@@ -5,27 +5,25 @@ var h = Cycle.h;
 
 function vrenderHeader(people) {
   return h('header#header', [
-    h('h1', 'queue')
+    h('h1', 'Awesome Human Queue')
   ]);
 }
 
 function vrenderQueuedPerson(name) {
   return h('li', [
-    h('label', name),
-    h('button', {
+    h('button.button-primary', {
       attributes: {'data-name': name},
       onclick: 'removeFromQueue$'
-    }, 'Remove')
+    }, name)
   ]);
 }
 
 function vrenderListPerson(name) {
   return h('li', [
-    h('label', name),
     h('button', {
       attributes: {'data-name': name},
       onclick: 'addToQueue$'
-    }, 'Add')
+    }, name)
   ]);
 }
 
@@ -33,28 +31,34 @@ function vrenderList(people) {
   var unqueued = people.list.filter(function(person) {
     return people.queue.indexOf(person) === -1
   });
-  return h('ul#unqueued', {
-      style: {'display': unqueued.length ? '' : 'none'}
-    },
-    unqueued.map(vrenderListPerson)
-  );
+  return h('div#unqueued.one-half.column', [
+    h('h3', 'People'),
+    h('ul', {
+        style: {'display': unqueued.length ? '' : 'none'}
+      },
+      unqueued.map(vrenderListPerson)
+    )
+  ]);
 }
 
 function vrenderQueue(people) {
-  return h('ul#queued', {
-      style: {'display': people.queue.length ? '' : 'none'}
-    },
-    people.queue.map(vrenderQueuedPerson)
-  );
+  return h('div#queued.one-half.column', [
+    h('h3', 'Waiting'),
+    h('ul', {
+        style: {'display': people.queue.length ? '' : 'none'}
+      },
+      people.queue.map(vrenderQueuedPerson)
+    )
+  ]);
 }
 
 var QueueView = Cycle.createView(function (model) {
   return {
     vtree$: model.get('people$')
       .map(function (people) {
-        return h('div', [
+        return h('div.container', [
           vrenderHeader(people),
-          h('section#main', [
+          h('section#main.row', [
             vrenderList(people),
             vrenderQueue(people)
           ])
